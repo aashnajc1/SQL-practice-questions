1051c3b78d48a73f2239dc305d7ccc20f3be0ad4
Aashna J Chandrashekhar <105263888+aashnajc1@users.noreply.github.com>
2022-10-09 22:07:29 +0530

    Update sql practice hard.sql

--- sql practice hard.sql
+++ sql practice hard.sql
@@ -25,3 +25,16 @@ where diagnosis = 'Epilepsy' and ph.first_name = 'Lisa';
 
 /* 4) Each admission costs $50 for patients without insurance, and $10 for patients with insurance. All patients with an even patient_id have insurance.
 Give each patient a 'Yes' if they have insurance, and a 'No' if they don't have insurance. Add up the admission_total cost for each has_insurance group. */
+select 
+case when patient_id % 2 = 0 then 
+    'Yes'
+else
+    'No'
+end as has_insurance,
+sum(case when patient_id%2 = 0 then 
+    10
+else
+    50
+end) as cost_after_insurance
+from admissions
+group by has_insurance;
