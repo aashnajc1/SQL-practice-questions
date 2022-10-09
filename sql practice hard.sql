7ba7e0e68031057c922d24441ba2ed33cab84ae7
Aashna J Chandrashekhar <105263888+aashnajc1@users.noreply.github.com>
2022-10-09 22:21:52 +0530

    Update sql practice hard.sql

--- sql practice hard.sql
+++ sql practice hard.sql
@@ -38,3 +38,11 @@ else
 end) as cost_after_insurance
 from admissions
 group by has_insurance;
+
+/* 5) Show the provinces that has more patients identified as 'M' than 'F'. Must only show full province_name */
+SELECT pr.province_name
+FROM patients AS pa
+  JOIN province_names AS pr ON pa.province_id = pr.province_id
+GROUP BY pr.province_name
+HAVING
+  COUNT( CASE WHEN gender = 'M' THEN 1 END) > COUNT( CASE WHEN gender = 'F' THEN 1 END);
