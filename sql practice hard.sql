db89e20235e50b5290df2d9d44207ba7a86661c6
Aashna J Chandrashekhar <105263888+aashnajc1@users.noreply.github.com>
2022-10-11 18:16:47 +0530

    Update sql practice hard.sql

--- sql practice hard.sql
+++ sql practice hard.sql
@@ -54,3 +54,12 @@ else
 end) as cost_after_insurance
 from admissions
 group by has_insurance;*/ 
+ SELECT *
+FROM patients
+WHERE
+  first_name LIKE '__r%'
+  AND gender = 'F'
+  AND MONTH(birth_date) IN (2, 5, 12)
+  AND weight BETWEEN 60 AND 80
+  AND patient_id % 2 = 1
+  AND city = 'Kingston';
