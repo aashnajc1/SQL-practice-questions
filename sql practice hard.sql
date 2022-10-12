730fdc7fd0ac4361ea9792e0446fa94b66fc5aeb
Aashna J Chandrashekhar <105263888+aashnajc1@users.noreply.github.com>
2022-10-12 10:41:21 +0530

    Update sql practice hard.sql

--- sql practice hard.sql
+++ sql practice hard.sql
@@ -65,3 +65,15 @@ WHERE
   AND city = 'Kingston';
 
  /* 7) Show the percent of patients that have 'M' as their gender. Round the answer to the nearest hundreth number and in percent form. */
+    SELECT CONCAT(
+    ROUND(
+      (
+        SELECT COUNT(*)
+        FROM patients
+        WHERE gender = 'M'
+      ) / CAST(COUNT(*) as float),
+      4
+    ) * 100,
+    '%'
+  ) as percent_of_male_patients
+FROM patients;
