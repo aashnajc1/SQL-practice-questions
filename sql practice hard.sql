c801fe41c2919f8d04a8657f043031b6d15b448c
Aashna J Chandrashekhar <105263888+aashnajc1@users.noreply.github.com>
2022-10-13 18:41:16 +0530

    Update sql practice hard.sql

--- sql practice hard.sql
+++ sql practice hard.sql
@@ -79,3 +79,9 @@ WHERE
 FROM patients;
 
 /* 8) For each day display the total amount of admissions on that day. Display the amount changed from the previous date. */
+SELECT
+ admission_date,
+ count(admission_date) as admission_day,
+ count(admission_date) - LAG(count(admission_date)) OVER(ORDER BY admission_date) AS admission_count_change 
+FROM admissions
+ group by admission_date
