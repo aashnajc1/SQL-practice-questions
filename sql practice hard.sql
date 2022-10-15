90dc2a139dda6061bc150d0d6d07798253ad93cb
Aashna J Chandrashekhar <105263888+aashnajc1@users.noreply.github.com>
2022-10-15 23:07:04 +0530

    Update sql practice hard.sql

--- sql practice hard.sql
+++ sql practice hard.sql
@@ -85,3 +85,20 @@ SELECT
  count(admission_date) - LAG(count(admission_date)) OVER(ORDER BY admission_date) AS admission_count_change 
 FROM admissions
  group by admission_date
+    
+/* 9) All patients who have gone through admissions, can see their medical documents on our site. 
+Those patients are given a temporary password after their first admission. Show the patient_id and temp_password.
+
+The password must be the following, in order:
+1. patient_id
+2. the numerical length of patient's last_name
+3. year of patient's birth_date */
+SELECT
+  DISTINCT P.patient_id,
+  CONCAT(
+    P.patient_id,
+    LEN(last_name),
+    YEAR(birth_date)
+  ) AS temp_password
+FROM patients P
+  JOIN admissions A ON A.patient_id = P.patient_id;
